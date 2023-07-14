14a49de60afae2a8384b892566372b391ead7078
jinqili0310 <jinqi.li.310@gmail.com>
2023-07-13 17:17:44 -0700

    link

--- src/app/tinctures/page.js
+++ src/app/tinctures/page.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-06-28 11:57:07
  * @LastEditors: jinqili0310 jinqi.li.310@gmail.com
- * @LastEditTime: 2023-07-12 15:24:28
+ * @LastEditTime: 2023-07-13 17:17:06
  * @FilePath: \felho-fullpage\src\app\tinctures\page.js
  */
 'use client'
@@ -18,19 +18,19 @@ export default function Tinctures() {
             h1: `Felhö DREAM`,
             h5: "Full Spectrum Tincture",
             p: "Our Dream tincture will have you calm and relaxed - ready for a good night's sleep! The combination of CBD + CBN provides a calm and relaxing experience that puts your head in the right place for a full-8 hours of rest.",
-            url: "https://www.felho.com/dream.html",
+            url: "https://www.felho.com/dream-full-spectrum-cbd-tincture.html",
         },
         {
             h1: "Felhö RELAX",
             h5: "Hemp Supplement",
             p: "With 1000mg of Broad-Spectrum CBD oil, you will feel lighter than air. Our CBD tincture provides the perfect effects for those who are looking to relax after a long day of work or help with any stressful situation.",
-            url: "https://www.felho.com/relax.html",
+            url: "https://www.felho.com/relax-full-spectrum-cbd-tincture.html",
         },
         {
             h1: "Felhö VITAL",
             h5: "Hemp Supplement",
             p: "With the carefully curated combination of CBD and CBG, you will feel calm, comfortable, and uplifted. CBG delivers an optimal sense of relief from sore muscles/ joints due to the anti-inflammatory nature of CBG. On top of that, our wellness blend acts as a great hunger manipulator to help those looking to increase their appetite.",
-            url: "https://www.felho.com/vital.html",
+            url: "https://www.felho.com/vital-full-spectrum-cbd-tincture.html",
         },
     ];
 
